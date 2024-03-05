920bbc558439f5d8cc0222be6192013eb67d28e5
Polly <poliana_elen@hotmail.com>
2024-03-05 09:56:06 -0300

    style: Inserindo MUI

--- src/components/Home/sections/Hero/Hero.tsx
+++ src/components/Home/sections/Hero/Hero.tsx
@@ -1,9 +1,42 @@
+import { Button, Container, Grid, Typography, styled } from "@mui/material"
+import Avatar from '../../../../assets/images/avatar.jpg'
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import EmailIcon from '@mui/icons-material/Email';
 
 
 const Hero = () => {
+  const StyleHero = styled('div')(() => ({
+    background: '#000000',
+    height: '100vh',
+    padding: 0
+  }))
+  const StyleImage = styled('img')(() => ({
+    width: '100%',
+    borderRadius: '50%'
+  }))
   return (
     <>
-    Hero
+    <StyleHero>
+      <Container>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={4}>
+                <StyleImage src={Avatar} alt="" />
+          </Grid>
+          <Grid item xs={12} md={4}>
+                <Typography color='primary' variant="h1" textAlign='center'>Elen Poliana</Typography>
+                <Typography color='primary' variant="h2"  textAlign='center'>Developer</Typography>
+                <Grid container display='flex' justifyContent='center'>
+                <Grid item xs={12} md={6} display='flex' justifyContent='center'>
+                   <Button> <ArrowDownwardIcon/> Dowload CV</Button>
+                </Grid>
+                <Grid item xs={12} md={6} display='flex' justifyContent='center'>
+                  <Button> <EmailIcon/> Contact CV</Button>
+                </Grid>
+                </Grid>
+          </Grid>
+        </Grid>    
+      </Container>
+    </StyleHero>
     </>
   )
 }
